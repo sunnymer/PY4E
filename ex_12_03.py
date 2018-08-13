fce331ab22d3a699548b60b5c5aab066488640ba
sunnymer <33538474+sunnymer@users.noreply.github.com>
2018-08-13 21:13:08 +0800

    web

--- ex_12_03.py
+++ ex_12_03.py
@@ -49,7 +49,7 @@ The answer to the assignment for this execution is "Anayah".
 from urllib.request import urlopen
 from bs4 import BeautifulSoup
 import ssl
-import re
+
 
 ctx = ssl.create_default_context()
 ctx.check_hostname = False
